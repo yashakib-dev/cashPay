3feb033c37cb689bd0dbc7fb3fc6350380aae0ea
yashakib-dev <yashakib.dev@gmail.com>
2026-02-20 15:27:27 +0600

    New Payoo Project

---
+++ script/script.js
@@ -0,0 +1,22 @@
+document.getElementById("login-button")
+        .addEventListener("click", function(){
+                const numberInput = document.getElementById("mobile-number");
+                const validNumber = numberInput.value;
+                if(validNumber.length != 11){
+                    alert("Number must be 11 digit.")
+                    return;
+                }
+                console.log(validNumber);
+
+                const pin = document.getElementById("pin-number");
+                const pinNumber = pin.value;
+                if( validNumber === "01823456789" && pinNumber === "0987"){
+                    alert("Login successful😍");
+                    window.location.assign("home.html");
+                }
+                else{
+                    alert("Login failed😞. Try again.");
+                    return;
+                }
+        })
+
