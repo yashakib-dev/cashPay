e6bd13b7e5a988d344be742ad7f00ed8558a3b0f
yashakib-dev <yashakib.dev@gmail.com>
2026-02-21 16:31:42 +0600

    Almost done

--- script/history.js
+++ script/history.js
@@ -2,7 +2,11 @@
             .addEventListener("click", function(){
                 console.log("btn clicked")
                 const bankAccount = getValueFromInput("add-money-bank");
+                const company = getValueFromInput("institute");
+              
                 const addAmount = getValueFromInput("add-money-amount");
+                const payAmount = getValueFromInput("bill-amount");
+
                 const history = document.getElementById("history");
                 console.log(history);
                 const newHistory = document.createElement("div");
@@ -11,7 +15,9 @@
                 <div class="flex gap-4 w-11/12 mx-auto pt-4  rounded-xl p-4 shadow bg-base-100">
                 <div class="flex justify-between w-11/12 mx-auto pb-6">
                     ${addAmount} Dollar added successfully from ${bankAccount} at ${ new Date()}
+                   
                 </div>
+
                  </div>   
      
                 `;
